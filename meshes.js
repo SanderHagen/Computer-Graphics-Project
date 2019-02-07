3cb6445c106c275b1f691deeef6079d5442c9777
SanderHagen <sanderhagen@outlook.com>
2019-02-07 17:47:03 +0100

    pyramid

--- meshes.js
+++ meshes.js
@@ -19,3 +19,25 @@ function CreateBase(x,z){
     cube.position.set(x,5,z);
     return cube;
 }
+
+function createPyramid(){
+    var geometry = new THREE.Geometry();
+
+    geometry.vertices = [
+        new THREE.Vector3( 0, 0, 0 ),
+        new THREE.Vector3( 0, 1, 0 ),
+        new THREE.Vector3( 1, 1, 0 ),
+        new THREE.Vector3( 1, 0, 0 ),
+        new THREE.Vector3( 0.5, 0.5, 1 )
+    ];
+
+    geometry.faces = [
+        new THREE.Face3( 0, 1, 2 ),
+        new THREE.Face3( 0, 2, 3 ),
+        new THREE.Face3( 1, 0, 4 ),
+        new THREE.Face3( 2, 1, 4 ),
+        new THREE.Face3( 3, 2, 4 ),
+        new THREE.Face3( 0, 3, 4 )
+    ];    
+    return geometry;
+}
